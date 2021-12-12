3b37d5173991aac09fa469f0017266a8b5db4261
Matt Kolbach <mattkolbach@yahoo.com>
2021-12-11 20:37:47 -0600

    bug, player-name, fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,8 +68,18 @@ var fight = function (enemy) {
   }
 };
 
+//function to set player name
+var getPlayerName = function () {
+  var name = "";
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+}
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
